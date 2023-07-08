94ad8697178347ca207d43bcc973a6021a964a7a
Mac <mact0003@gmail.com>
2023-07-08 17:26:55 -0400

    added script for canvas and style, also samples for animation

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContex('2d');
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+
+function animate(){
+	
+}
