e46a694e77e078e514147ce25f1f534ece0b89bc
Mac <mact0003@gmail.com>
2023-07-10 23:58:44 -0400

    added position variable to animate through canvas

--- script.js
+++ script.js
@@ -10,18 +10,14 @@ const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
-const stagerFrames = 5;
+const stagerFrames = 0;
 
 function animate(){
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	//ctx.fillRect(50, 50, 100, 100);
-	//ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+	//gameframe divided by stagerframes means we will have to increase gameframe 5 times before we get to run
+	let position = Math.floor(gameFrame/stagerFrames) % 6;
 	ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-	if (gameFrame % stagerFrames == 0) {
-		if (frameX < 6) frameX++;
-		else frameX = 0;
-
-	}
+	
 
 	gameFrame++;
 	requestAnimationFrame(animate);
