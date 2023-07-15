1d9b34ddeed55bfebc30b63bce8d071a7a60c6aa
Mac <mact0003@gmail.com>
2023-07-15 16:07:06 -0400

    Added new abilities into array, no change to animate funcion needed so far

--- script.js
+++ script.js
@@ -20,6 +20,38 @@ const animationStates = [
 		name: 'jump',
 		frames: 7,
 	}
+	{
+		name: 'fall',
+		frames: 9,
+	}
+	{
+		name: 'run',
+		frames: 9,
+	}
+	{
+		name: 'dizzy',
+		frames: 11,
+	}
+	{
+		name: 'sit',
+		frames: 5,
+	}
+	{
+		name: 'roll',
+		frames: 7,
+	}
+	{
+		name: 'bite',
+		frames: 7,
+	}
+	{
+		name: 'ko',
+		frames: 12,
+	}
+	{
+		name: 'getHit',
+		frames: 7,
+	}
 ];
 //forEach executes provided functions once for each array, Es6 syntax arrow function
 animationStates.forEach((state, index) => {
